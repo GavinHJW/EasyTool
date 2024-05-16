03225d3ac132e53903b8d961b5c16e23548ad144
hejiawenn <11566172+hejiawenn@user.noreply.gitee.com>
2024-05-17 00:15:44 +0800

    提交注释

--- src/renderer/preload.d.ts
+++ src/renderer/preload.d.ts
@@ -1,5 +1,5 @@
 import { ElectronHandler } from '../main/preload';
-
+// preload.d.ts 文件的作用就是为 Electron 渲染器进程中的 preload 脚本提供类型定义，使得开发者在编写 TypeScript 代码时能够更加方便地与主进程进行通信，以及避免一些潜在的类型错误
 declare global {
 	// eslint-disable-next-line no-unused-vars
 	interface Window {
